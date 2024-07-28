068c616f5b5bcd57392dfb48d80790f47c932528
Gabriel Curti <gabrielcurti.dev@outlook.com>
2024-07-28 01:53:23 -0300

    mexendo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let btnCreatlist = document.querySelector('.btn-creatlist')
 let createNamelist = document.querySelector('.create-namelist')
 let navbarLeft = document.querySelector('.navbar-left')
+let containerTodo = document.querySelector('.todo-Name')
 
 btnCreatlist.addEventListener('click', ()=> {
 let inputValue = document.querySelector('.input-namelist').value.trim()
@@ -8,10 +9,20 @@ let inputValue = document.querySelector('.input-namelist').value.trim()
     if(inputValue !== ''){
         let newElement = document.createElement('div')
         newElement.classList.add('create-namelist')
-        navbarLeft.appendChild('newElement')
-
+        
         let todoName = document.createElement('div')
-
+        todoName.classList.add('todo-Name')
+        createNamelist.appendChild(todoName)
+        
+        let todoTitle = document.createElement('h2')
+        todoTitle.innerText = inputValue
+        containerTodo.appendChild(todoTitle)
+        
+        let todoDelName = document.createElement('div')
+        todoDelName.classList('del-namelist')
+        containerTodo.appendChild(todoDelName)
+        
+        navbarLeft.appendChild(newElement)
 
   
     }
